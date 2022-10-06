d635a46a89e1936d807244107a50acb5821593b2
Ian Shehadeh <IanShehadeh2020@gmail.com>
2022-10-06 13:16:39 -0400

    basic validation for podcast create form

--- cms/app/src/main/java/seahawkradio/cms/PodcastController.java
+++ cms/app/src/main/java/seahawkradio/cms/PodcastController.java
@@ -1,16 +1,60 @@
 package seahawkradio.cms;
 
+import io.javalin.core.validation.JavalinValidation;
 import io.javalin.http.Handler;
 
+import org.owasp.html.HtmlPolicyBuilder;
+import org.owasp.html.PolicyFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class PodcastController {
     private static final Logger LOG = LoggerFactory.getLogger(PodcastController.class);
+    private static final PolicyFactory PODCAST_DESCRIPTION_SANATIZER =
+            new HtmlPolicyBuilder()
+                    .allowElements("a", "h1", "h2", "h3", "ul", "ol", "li")
+                    .toFactory();
+
+    private static final Boolean validateLanguage(String alpha2_in) {
+        for (var lang : Language.ALL) {
+            if (lang.alpha2().isPresent() && lang.alpha2().get().contentEquals(alpha2_in)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
 
     public static final Handler createPodcastEndpoint =
             ctx -> {
                 LOG.atInfo().addKeyValue("params", ctx.formParamMap()).log("create podcast req");
-                ctx.result("todo");
+                var language =
+                        ctx.formParamAsClass("language", String.class)
+                                .check(
+                                        PodcastController::validateLanguage,
+                                        "not a valid language code");
+                var explicit = ctx.formParamAsClass("explicit", Boolean.class);
+                var description = ctx.formParamAsClass("description", String.class);
+                var title = ctx.formParamAsClass("title", String.class);
+                var copyright = ctx.formParamAsClass("copyright", String.class);
+
+                final var formParamErrors =
+                        JavalinValidation.collectErrors(
+                                language, explicit, description, title, copyright);
+                if (!formParamErrors.isEmpty()) {
+                    LOG.atInfo()
+                            .addKeyValue("errors", formParamErrors)
+                            .log("rejecting podcast create request");
+                    ctx.result("invalid request").status(400);
+                    return;
+                }
+
+                var sanatizedDescription =
+                        PodcastController.PODCAST_DESCRIPTION_SANATIZER.sanitize(description.get());
+                LOG.atInfo()
+                        .addKeyValue("description", description.get())
+                        .addKeyValue("sanatizedDescription", sanatizedDescription)
+                        .log("sanatized description");
+                ctx.result("ok");
             };
 }
